5bb59574085d53e97e58749dcb29fb5cf7f60f91
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-21 03:43:07 -0500

    added tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import LandingPage from './LandingPage.tsx'
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LandingPage />

25cab52ecdaf6c61cc33e4e5239a1a4032a32d51
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-22 15:13:20 -0500

    testing out api calling

--- bark/app/page.tsx
+++ bark/app/page.tsx
@@ -1,14 +1,23 @@
+'use client';
+
 export default async function page(){
   const res = await fetch('http://localhost:3000/api/test', {cache:'no-store'});
   const text = await res.text();
+
   
   return(
     <>
-    <input placeholder="Enter Pokemon: " 
+
+    
+    <input placeholder="Enter Pokemon:" 
     className="text-7xl border-2 rounded-2xl"
+    onChange={()=>console.log('changed!')} 
     ></input>
+
+
     <button 
     className="text-7xl border-2 rounded-2xl "
+    onClick={()=>console.log('clicked!')}
     >Search</button>
     </>
   )

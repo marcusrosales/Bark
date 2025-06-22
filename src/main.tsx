ef92d5030a040ecbc947743e58961ca27455ca0a
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-22 03:11:44 -0500

    adding file structuring

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import LandingPage from './LandingPage.tsx'
+import LandingPage from './Layout/LandingPage.tsx'
 
 
 createRoot(document.getElementById('root')!).render(

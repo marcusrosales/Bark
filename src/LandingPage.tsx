59b12243c0d2d98eabdeb968619f3b492d130dfb
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-22 03:10:11 -0500

    changed project name to bark, is very simple like project scope

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,13 +1,19 @@
 function LandingPage(){
+
   return(
     <>
-    
-    <h1 className="text-9xl">DogPark</h1>
 
+    <h1 className="text-9xl">Bark</h1>
 
+    <h2 className="" >See Photos, Details, And find </h2>
 
     </>)
 }
+
+
+
+
+
 
 
 

a122e83712a8f9a1fb2534ba849f5274e898c245
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-22 05:20:01 -0500

    testing api and routing its pretty cool its really simple on here

--- bark/app/api/test/route.js
+++ bark/app/api/test/route.js
@@ -1,3 +1,3 @@
 export async function GET(){
-    return new Respsone('hello world!')
+    return new Response('hello world!')
 }
--- bark/app/page.tsx
+++ bark/app/page.tsx
@@ -1,1 +1,15 @@
-export 
+export default async function page(){
+  const res = await fetch('http://localhost:3000/api/test', {cache:'no-store'});
+  const text = await res.text();
+  
+  return(
+    <>
+    <input placeholder="Enter Pokemon: " 
+    className="text-7xl border-2 rounded-2xl"
+    ></input>
+    <button 
+    className="text-7xl border-2 rounded-2xl "
+    >Search</button>
+    </>
+  )
+}

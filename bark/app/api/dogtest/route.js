9e42e1e35490db4014a34cda187738c416401d53
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-22 21:43:34 -0500

    Messing with code trying to fix

--- bark/app/api/dogtest/route.js
+++ bark/app/api/dogtest/route.js
@@ -1,16 +1,14 @@
+const token = process.env.PETFINDER_TOKEN
 
-let key = c
-export async function GET(request){
-    const res = await fetch('https://api.petfinder.com/v2/animals',
+export default async function GET(request){
+    const res = await fetch('https://api.petfinder.com/v2/animals',{
         method:"GET",
         headers:{
-            'Authorization':
+            Authorization: `Bearer ${token}`
         }
-    );
-    const data = await res.json()
-
-    
-
+    })
+    const data = await res.json();
 
     return new Response(JSON.stringify(data))
+
 }
--- bark/app/page.tsx
+++ bark/app/page.tsx
@@ -1,29 +1,10 @@
 'use client';
 
-export default async function page(){
-  const res = await fetch('http://localhost:3000/api/dogtest');
-  const text = await res.text();
 
-  
+export default async function page(){
   return(
     <>
-    <input placeholder="Enter Pokemon:" 
-    className="text-7xl border-2 rounded-2xl"
-    onChange={()=>console.log('changed!')} 
-    ></input>
-
-
-    <button 
-    className="text-7xl border-2 rounded-2xl "
-    onClick={()=>console.log('clicked!')}
-    >Search</button>
-
-
-    <h1>{text}</h1>
-
+    <h1>hello</h1>
     </>
-
-
-
 )
 }
